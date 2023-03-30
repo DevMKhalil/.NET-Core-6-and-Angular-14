ba610b00bba4975f323293c34826d87ab0fa8069
DevMKhalil <dev.mohammedkhalil@gmail.com>
2023-03-30 05:07:03 +0200

    Add Lazy Loading To AppRoutingModule

--- HealthCheck/HealthCheck/src/app/app-routing/app-routing.module.ts
+++ HealthCheck/HealthCheck/src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
-import { FetchDataComponent } from '../fetch-data/fetch-data.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'fetch-data', component: FetchDataComponent }
+  {
+    path: '',
+    loadComponent: () => import('../home/home.component').then(mod => mod.HomeComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'fetch-data',
+    loadComponent: () => import('../fetch-data/fetch-data.component').then(mod => mod.FetchDataComponent)
+  }
 ];
 
 @NgModule({
